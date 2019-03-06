63883d56bce7791e3704e4f42c2a50706e7076cf
ashutoshkumarsingh <ashutoshsingh.0207@gmail.com>
2019-03-07 02:48:39 +0530

    search ui

--- App.js
+++ App.js
@@ -1,50 +1,76 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    TouchableHighlight
+} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+class App extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    render() {
+        return (
+            <View style={styles.root}>
+                <View style={styles.topContainer}>
+                    <TextInput style={styles.textInput}
+                        placeholder='Whats in your mind?'
+                        onChangeText={(searchText) => this.setState({searchText})}
+                    />
+                    <TouchableHighlight style={styles.searchButton}
+                        underlayColor='#007AFF'>
+                        <Text style={styles.buttonText}>Search</Text>
+                    </TouchableHighlight>
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+                </View>
+            </View>
+        );
+    }
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    root: {
+        flex: 1,
+        alignItems: 'center' ,
+    },
+    topContainer: {
+        margin: 10,
+        borderRadius: 10,
+        flex: 2,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        marginBottom: 20
+    },
+    textInput: {
+        fontSize: 30,
+        fontFamily: 'helvetica',
+        fontWeight: 'bold',
+        alignSelf: 'center',
+        textAlign: 'center',
+        color: '#007AFF',
+        marginTop: 30,
+        height: 64, width: 300
+    },
+    searchButton: {
+        marginTop: 20,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#007AFF',
+        borderRadius: 20,
+        height: 60, width: 150
+    },
+    buttonText: {
+        fontSize: 24,
+        fontFamily: 'helvetica',
+        fontWeight: 'bold',
+        color: '#FFF',
+        textAlign: 'center',
+    }
+})
+
+module.exports = App;
